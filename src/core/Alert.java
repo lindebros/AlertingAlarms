2e09473b8ab95809360fece42008f7c852a092de
lindebros <math.linde1994@gmail.com>
2023-03-01 21:41:27 +0100

    I have created the basis for an alarm core object

---
+++ src/core/Alert.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Alert {
+    private String name;
+    private String description;
+
+    public Alert(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
+import core.Alert;
+
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Alert someAlert = new Alert("Alert1", "This is my first alarm");
+        System.out.println(String.format("I have created an alert \"%s\" with the following description: %s", someAlert.getName(), someAlert.getDescription()));
+
     }
 }
